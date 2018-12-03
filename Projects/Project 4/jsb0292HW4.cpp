35ae9e2b201d367c4e094387c80f0dcac3023da5
Jordan Bursik <jsbursik@gmail.com>
2018-12-03 00:20:31 -0600

    This works so far

--- Projects/Project 4/jsb0292HW4.cpp
+++ Projects/Project 4/jsb0292HW4.cpp
@@ -1,38 +1,39 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 using namespace std;
 
-enum TYPE {LOCKED = 'X', EMPTY = ' ', CROSS = '+', HORIZONTAL = '-', VERTICAL = '|'};
+enum TYPE {EMPTY = ' ', LOCKED = 'X', CROSS = '+', VERTICAL = '|', HORIZONTAL = '-'};
 enum COLOR {RED, BLUE, NONE};
 
-struct Tile{
+struct Tile {
   TYPE type = EMPTY;
   COLOR color = NONE;
   int position;
 };
 
-void openBoard(ifstream& in_s, Tile **t, int&x, int&y);
-void printBoard(Tile **t, int x, int y);
+void openBoard(ifstream& in_s, Tile **t, int& x, int& y);
 
 int main(){
-  Tile **tiles;
+  cout << "***************************************************" << endl;
+  cout << "\tComputer Science and Engineering" << endl;
+  cout << "\t CSCE 1030 - Computer Science I" << endl;
+  cout << "Jordan Bursik    jsb0292    jordanbursik@my.unt.edu" << endl;
+  cout << "***************************************************" << endl;
+  
   ifstream in_s;
+  Tile **tiles;
   int x = 0, y = 1;
 
   openBoard(in_s, tiles, x, y);
 
   tiles = new Tile*[y];
-
   for (int i = 0; i < y; i++){
     tiles[i] = new Tile[x];
   }
 
-  printBoard(tiles, x, y);
-
   cout << x << endl;
   cout << y << endl;
-
+  
   in_s.close();
   for (int i = 0; i < y; i++){
     delete[] tiles[i];
@@ -41,33 +42,21 @@ int main(){
   return 0;
 }
 
-
-void openBoard(ifstream& in_s, Tile **t, int&x, int&y){
+void openBoard(ifstream& in_s, Tile **t, int& x, int& y){
   char filename[20], c;
-
-  cout << "Enter filename: "; cin >> filename;
+  cout << "Enter the filename for the board: "; cin >> filename;
   in_s.open(filename);
   while(in_s.fail()){
-    cout << "Failed to open file, try again: "; cin >> filename;
+    cout << "File failed to open, please enter another file name: "; cin >> filename;
     in_s.open(filename);
   }
 
   in_s.get(c);
   while(!in_s.eof()){
-    x = ((c == 'X') || (c == ' ')) ? ++x : x;
-    y = (c == '\n') ? ++y : y;
+    x = ((c==LOCKED) || (c==EMPTY)) ? ++x: x;
+    y = (c == '\n') ? ++y: y;
     in_s.get(c);
   }
-
   x /= y;
 
-}
-
-void printBoard(Tile **t, int x, int y){
-  for (int i = 0; i < y; i++){
-    for (int j = 0; j < x; j++){
-      cout << static_cast<char>(t[i][j].type) << " ";
-    }
-    cout << endl;
-  }
 }
